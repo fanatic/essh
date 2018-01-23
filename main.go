0a649962678fde0e5103006129f5fdd5a9c97c20
Jason Parrott <fanatic+github@gmail.com>
2018-01-22 21:09:09 -0500

    initial

---
+++ main.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"syscall"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+var (
+	cmd  = "/usr/bin/sudo"
+	args = []string{"-u", "ec2-user", "ssh"}
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalln("Usage: ./essh <filter> [<command>]")
+	}
+
+	// Grab our instances
+	instances := fetchInstances()
+	// instances := []*ec2.Instance{
+	// 	&ec2.Instance{
+	// 		PrivateIpAddress: String("192.168.1.1"),
+	// 		Tags: []*ec2.Tag{
+	// 			&ec2.Tag{
+	// 				Key:   String("Name"),
+	// 				Value: String("my-instance"),
+	// 			},
+	// 		},
+	// 	},
+	// 	&ec2.Instance{
+	// 		PrivateIpAddress: String("192.168.1.2"),
+	// 		Tags: []*ec2.Tag{
+	// 			&ec2.Tag{
+	// 				Key:   String("Name"),
+	// 				Value: String("my-app"),
+	// 			},
+	// 		},
+	// 	},
+	// }
+
+	// Filter by argument (env, region, zone, app)
+	filtered := filterInstances(os.Args[1], instances)
+
+	if len(filtered) == 0 {
+		fmt.Println("No match found")
+		return
+	}
+
+	// One result and no args - exec ssh and give control back to the user
+	if len(os.Args) == 2 && len(filtered) == 1 {
+		interactive(filtered[0])
+		return
+	}
+
+	// If command is present, execute on all
+	if len(os.Args) > 2 {
+		names := []string{}
+		for _, i := range filtered {
+			names = append(names, instanceTag(i, "Name"))
+		}
+		fmt.Printf("Matched nodes: %s\n", strings.Join(names, ", "))
+		confirm()
+
+		for _, i := range filtered {
+			fmt.Printf("\n=== %s\n", instanceTag(i, "Name"))
+			allArgs := append([]string{*i.PrivateIpAddress}, os.Args[1:]...)
+			err := exec.Command(cmd, append(args, allArgs...)...).Run()
+			if err != nil {
+				log.Fatalf("error executing command: %s", err.Error())
+			}
+		}
+		return
+	}
+
+	// If command isn't present, request user filter down arguments
+	i := giveChoice(filtered)
+	interactive(i)
+}
+
+func fetchInstances() []*ec2.Instance {
+	ec2svc := ec2.New(session.New())
+	params := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			{
+				Name:   aws.String("instance-state-name"),
+				Values: []*string{aws.String("running")},
+			},
+		},
+	}
+	resp, err := ec2svc.DescribeInstances(params)
+	if err != nil {
+		fmt.Println("there was an error listing instances in", err.Error())
+		log.Fatal(err.Error())
+	}
+
+	instances := []*ec2.Instance{}
+	for idx := range resp.Reservations {
+		instances = append(resp.Reservations[idx].Instances, instances...)
+	}
+	return instances
+}
+
+func filterInstances(filter string, instances []*ec2.Instance) []*ec2.Instance {
+	filtered := instances[:0]
+	for _, i := range instances {
+		if tagsHasFilter(i.Tags, filter) {
+			filtered = append(filtered, i)
+			continue
+		}
+	}
+	return filtered
+}
+
+func tagsHasFilter(tags []*ec2.Tag, filter string) bool {
+	for _, tag := range tags {
+		if strings.Contains(tag.GoString(), filter) {
+			return true
+		}
+	}
+	return false
+}
+
+func instanceTag(i *ec2.Instance, key string) string {
+	for _, tag := range i.Tags {
+		if *tag.Key == key {
+			return *tag.Value
+		}
+	}
+	return ""
+}
+
+func interactive(i *ec2.Instance) {
+	allArgs := append([]string{cmd}, args...)
+	syscall.Exec(cmd, append(allArgs, *i.PrivateIpAddress), []string{})
+}
+
+func giveChoice(filtered []*ec2.Instance) *ec2.Instance {
+	fmt.Println("Multiple matches:")
+	for idx, i := range filtered {
+		fmt.Printf("[%d] %s %s\n", idx, instanceTag(i, "Name"), *i.PrivateIpAddress)
+	}
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Pick [0]: ")
+	choice, _ := reader.ReadString('\n')
+	choice = strings.TrimSpace(choice)
+	if choice == "" {
+		return filtered[0]
+	}
+	idx, err := strconv.Atoi(choice)
+	if err != nil || idx < 0 || idx > len(filtered)-1 {
+		fmt.Println("Bad input")
+		os.Exit(1)
+	}
+	return filtered[idx]
+}
+
+func confirm() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Are you sure you want to run this on these nodes? Ctrl+C to cancel?")
+	reader.ReadString('\n')
+}
+
+func String(str string) *string {
+	return &str
+}
